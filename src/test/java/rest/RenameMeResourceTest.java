ab180676df760881ec67e0fef6ca0c6255690f25
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-11-07 10:23:01 +0100

    test fra Marie

--- src/test/java/rest/RenameMeResourceTest.java
+++ src/test/java/rest/RenameMeResourceTest.java
@@ -28,6 +28,7 @@ public class RenameMeResourceTest {
     private static final String SERVER_URL = "http://localhost/api";
     private static RenameMe r1, r2;
 
+
     static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
