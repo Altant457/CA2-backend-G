6d12b3fe9dd2488530a05c876d249fc0138a09e0
Marie Fiskbæk <marie.fiskbak@gmail.com>
2022-11-07 11:39:29 +0100

    test Marie

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -50,6 +50,7 @@ public class User implements Serializable {
   public User() {}
 
   //TODO Change when password is hashed
+
   // public boolean verifyPassword(String pw){
 //        return(BCrypt.checkpw(pw, this.userPass));
 //    }
